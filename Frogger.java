50bcff6bb6ab815f29b18fb2508446601cb513c8
Rainford <rainfort@lancs.local>
2017-02-14 16:28:25 +0000

    Frog being attempted, trying to do add function.

--- Frogger.java
+++ Frogger.java
@@ -1,7 +1,19 @@
 public class Frogger
 {
-	public static void main(String[] args)
-	{
+	public static void main(String[] args){
 		GameArena arena = new GameArena(600,600);
-	}
+		Frog f = new Frog();
+
+		 arena.Frog.addBall(f);
+		 
+		 while(true)
+		 {
+			 double x = f.getXPosition();
+			 if (x < 500)
+				 f.setXPosition(x+1);
+			 else
+				 f.setXPosition(0);
+			 
+			 }	 
+	 }
 }
