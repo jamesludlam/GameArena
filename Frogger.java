38c262983466d466b205e20f86aed0c0660d53ed
Ludlam <ludlamj@lancs.local>
2017-02-21 17:01:57 +0000

    added new cars

--- Frogger.java
+++ Frogger.java
@@ -5,13 +5,19 @@ public class Frogger
 	{
 	    GameArena arena = new GameArena(800,800);
 		Car c1 = new Car(400,50);
+		Car c2 = new Car(700,50);
+		Car c3 = new Car(100,50);
 		Frog f = new Frog();
 		arena.addBall(f.f);
 		arena.addCar(c1);
+		arena.addCar(c2);
+		arena.addCar(c3);
 		
 		 while(true)
 		 {
 			c1.move();
+			c2.move();
+			c3.move();
 			arena.pause();
 			
 			 if(arena.upPressed())
